653bddb1f090786fb949e94ba94f7e96c4cded45
Deric Walintukan <deric.walintukan@gmail.com>
2019-02-11 10:26:10 +0700

    Change landing page and remove help

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -60,13 +60,10 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/docusaurus.svg`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('home')}>View Docs</Button>
           </PromoSection>
         </div>
       </SplashContainer>
@@ -190,16 +187,8 @@ class Index extends React.Component {
     };
 
     return (
-      <div>
+      <div className="homeSplashContainer">
         <HomeSplash siteConfig={siteConfig} language={language} />
-        <div className="mainContainer">
-          <Features />
-          <FeatureCallout />
-          <LearnHow />
-          <TryOut />
-          <Description />
-          <Showcase />
-        </div>
       </div>
     );
   }
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,6 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'home', label: 'Docs'},
-    {page: 'help', label: 'Help'},
   ],
 
   // If you have users set above, you add it here:
